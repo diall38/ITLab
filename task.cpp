40bf224331369d970a0c50475d33fe7ea5377ee9
diall38 <diall3819@gmail.com>
2021-11-04 01:09:00 +0300

    added std::chrono and generating double type

--- task.cpp
+++ task.cpp
@@ -2,11 +2,16 @@
 
 #include <iostream>
 #include <vector>
-#include <ctime>
+#include <chrono>
 #include <random>
+//#include <thread>
+
+
 #define N1 1024
 #define M2 1024
-using type = int;
+
+using type = double;
+
 type* matrix_multiplication_base(type* matr1, size_t n1, size_t m1, type* matr2, size_t n2, size_t m2) {
 	if (m1 != n2) {
 		std::cout << "Incorrect multiplication" << std::endl;
@@ -14,18 +19,21 @@ type* matrix_multiplication_base(type* matr1, size_t n1, size_t m1, type* matr2,
 	}
 
 	type* res_matr = new type[n1 * m2];
-	//#pragma omp parallel for //shared(matr1, matr2, res_matr) reduction (+:temp)
-	clock_t st = clock();
+	memset(res_matr, 0, n1 * m2 * sizeof(type));
+
+	auto start = std::chrono::high_resolution_clock::now();
 #pragma omp parallel for
-	for (size_t j = 0; j < m2; ++j) {
-		for (size_t i = 0; i < n1; ++i) {
-			type temp = 0;
-			for (size_t k = 0; k < m1; ++k)
-				temp += matr1[i * m1 + k] * matr2[k * m2 + j];
-			res_matr[i * m2 + j] = temp;
+	for (int j = 0; j < m2; ++j) {
+		for (int i = 0; i < n1; ++i) {
+#pragma omp simd
+			for (int k = 0; k < m1; ++k)
+				res_matr[i * m2 + j] += matr1[i * m1 + k] * matr2[k * m2 + j];
 		}
 	}
-	std::cout << "\nmatrix_multiplication_base\nTIME: " << (clock() - st) / (double)CLOCKS_PER_SEC << '\n';
+	auto end = std::chrono::high_resolution_clock::now();
+	std::cout << "\nmatrix_multiplication_base\nTIME: ";
+	std::chrono::duration<double> duration = end - start;
+	std::cout << duration.count() << std::endl;
 	return res_matr;
 }
 
@@ -34,23 +42,23 @@ type* matrix_multiplication_sqr_block(type* matr1, size_t n1, size_t m1, type* m
 		std::cout << "Incorrect multiplication" << std::endl;
 		return nullptr;
 	}
-
 	type* res_matr = new type[n1 * m2];
-	for (size_t i = 0; i < n1; i++)
-		for (size_t j = 0; j < m2; j++)
-			res_matr[i * m2 + j] = 0;
+	memset(res_matr, 0, n1 * m2 * sizeof(type));
 
-	clock_t st = clock();
+	auto start = std::chrono::high_resolution_clock::now();
 #pragma omp parallel for
 	for (int ib = 0; ib < n1; ib += block_sz)
 		for (int kb = 0; kb < m1; kb += block_sz)
 			for (int jb = 0; jb < m2; jb += block_sz)
 				for (int i = ib; i < ib + block_sz; ++i)
 					for (int k = kb; k < kb + block_sz; ++k)
+#pragma omp simd
 						for (int j = jb; j < jb + block_sz; ++j)
 							res_matr[i * m2 + j] += matr1[i * m1 + k] * matr2[k * m2 + j];
-	std::cout << "\nmatrix_multiplication_sqr_block\nTIME: " << (clock() - st) / (double)CLOCKS_PER_SEC << '\n';
-
+	auto end = std::chrono::high_resolution_clock::now();
+	std::cout << "\nmatrix_multiplication_sqr_block\nTIME: ";
+	std::chrono::duration<double> duration = end - start;
+	std::cout << duration.count() << std::endl;
 	return res_matr;
 }
 
@@ -61,28 +69,31 @@ type* matrix_multiplication_rect_block(type* matr1, size_t n1, size_t m1, type* 
 	}
 
 	type* res_matr = new type[n1 * m2];
-	for (size_t i = 0; i < n1; i++)
-		for (size_t j = 0; j < m2; j++)
-			res_matr[i * m2 + j] = 0;
+	memset(res_matr, 0, n1 * m2 * sizeof(type));
 
-	clock_t st = clock();
+	auto start = std::chrono::high_resolution_clock::now();
 #pragma omp parallel for
 	for (int jk = 0; jk < n1; jk += block_sz)
 		for (int ik = 0; ik < m1; ik += block_sz)
 			for (int j = jk; j < jk + block_sz; ++j)
 				for (int k = ik; k < ik + block_sz; ++k)
+#pragma omp simd
 					for (int i = 0; i < m2; ++i)
 						res_matr[j * m2 + i] += matr1[j * m1 + k] * matr2[k * m2 + i];
-	std::cout << "\nmatrix_multiplication_rect_block\nTIME: " << (clock() - st) / (double)CLOCKS_PER_SEC << '\n';
-
+	auto end = std::chrono::high_resolution_clock::now();
+	std::cout << "\nmatrix_multiplication_rect_block\nTIME: ";
+	std::chrono::duration<double> duration = end - start;
+	std::cout << duration.count() << std::endl;
 	return res_matr;
 }
 
 type* generate_matrix(const size_t N, const size_t M) {
+	double MAX = FLT_MAX, MIN = -MAX;
 	type* matrix = new type[N * M];
 	for (size_t i = 0; i < N; ++i)
-		for (size_t j = 0; j < M; ++j)
-			matrix[i * M + j] = rand() % 1009;
+		for (size_t j = 0; j < M; ++j) {
+			matrix[i * M + j] = MIN + (double)rand() / (double)(RAND_MAX / (MAX - MIN));
+		}
 
 	return matrix;
 }
@@ -100,10 +111,16 @@ int main()
 	type* matr1 = generate_matrix(N1, M2);
 	type* matr2 = generate_matrix(N1, M2);
 	type* ans = matrix_multiplication_rect_block(matr1, N1, M2, matr2, N1, M2, 32);
+
+	matr1 = generate_matrix(N1, M2);
+	matr2 = generate_matrix(N1, M2);
 	type* ans1 = matrix_multiplication_sqr_block(matr1, N1, M2, matr2, N1, M2, 256);
+
+	matr1 = generate_matrix(N1, M2);
+	matr2 = generate_matrix(N1, M2);
 	type* ans2 = matrix_multiplication_base(matr1, N1, M2, matr2, N1, M2);
 
-	for (size_t i = 0; i < N1; ++i)
+	/*for (size_t i = 0; i < N1; ++i)
 		for (size_t j = 0; j < M2; ++j) {
 			if (ans[i * M2 + j] != ans2[i * M2 + j]) {
 				std::cout << "FAILED RECTANGLE BLOCK\n";
@@ -115,11 +132,14 @@ int main()
 			}
 		}
 	std::cout << "PASSED\n";
+	*/
 
 	destructor(matr1);
 	destructor(matr2);
 	destructor(ans);
+	//destructor(ansp);
 	destructor(ans1);
+	//destructor(ans1p);
 	destructor(ans2);
 	return 0;
 }
